45302b89dd84955ee9df03207d8982c12ed91668
Anthony Cook <github@anthonymcook.com>
2015-10-04 02:36:45 -0500

    Moving the Uspec::Exec module into its own file.

---
+++ lib/uspec/exec.rb
@@ -0,0 +1,34 @@
+require 'pathname'
+require_relative '../uspec'
+
+module Uspec::Exec
+  module_function
+
+  def run_specs paths
+    paths = ['spec', 'uspec'] if paths.empty?
+    @pwd = Pathname.pwd
+
+    paths.each do |path|
+      run @pwd.join path
+    end
+  end
+
+  def run path
+    if path.directory? then
+      Pathname.glob(path.join('**', '**_spec.rb')).each do |spec|
+        run spec
+      end
+    elsif path.exist? then
+      puts "#{path.basename path.extname}:"
+      Uspec::DSL.instance_eval(path.read, path.to_s)
+    else
+      warn "spec/path not found: #{path}"
+    end
+  end
+
+  def usage
+    warn "uspec - minimalistic ruby testing framework"
+    warn "usage: #{File.basename $0} [<file_or_path>...]"
+    exit 1
+  end
+end
