c8f1441345400204350b2f01941425286534a939
Anthony Cook <github@anthonymcook.com>
2019-06-15 15:55:58 -0500

    Start trying to catch internal breakages.
    These are mostly caused by unexpected data types like BasicObject
    breaking assumptions about how objects should work. But it can also be
    caused by bugs in the test files or the tested files.

--- lib/uspec/dsl.rb
+++ lib/uspec/dsl.rb
@@ -15,6 +15,16 @@ module Uspec
 
       Uspec::Stats.results << result
       print ': ', formatter.colorize(result, caller), "\n"
+    rescue => error
+      message = <<-MSG
+        Uspec encountered an internal error, please report this bug!
+        https://github.com/acook/uspec/issues/new
+        #{error.class} : #{error.message}
+        #{error.backtrace.join "\n\t"}
+      MSG
+      puts
+      warn message
+      Uspec::Stats.results << [message, error]
     end
   end
 end
