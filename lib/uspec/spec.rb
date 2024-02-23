16f61febbd00d7a45268f89fa983a992b0c644e2
Anthony Cook <github@anthonymcook.com>
2024-02-22 23:40:48 -0600

    Update varnames and add error type to Spec object

--- lib/uspec/spec.rb
+++ lib/uspec/spec.rb
@@ -3,20 +3,21 @@ require_relative "result"
 module Uspec
   class Spec
 
-    def initialize dsl, description, &block
+    def initialize harness, description, &block
       @__uspec_description = description
-      @__uspec_harness = dsl
+      @__uspec_harness = harness
+      ns = harness.define
 
-      dsl.define.instance_variables.each do |name|
+      ns.instance_variables.each do |name|
         self.instance_variable_set(
           name,
-          @__uspec_harness.define.instance_variable_get(name)
+          ns.instance_variable_get(name)
         ) unless name.to_s.include? '@__uspec'
       end
 
-      dsl.define.methods(false).each do |name|
+      ns.methods(false).each do |name|
         self.define_singleton_method name do |*args, &block|
-          @__uspec_harness.define.send name, *args, &block
+          ns.send name, *args, &block
         end unless name.to_s.include? '__uspec'
       end
 
@@ -24,7 +25,7 @@ module Uspec
         self.define_singleton_method :__uspec_block, &block
       else
         self.define_singleton_method :__uspec_block do
-          raise "Uspec: No block provided for `#{@__uspec_description}`"
+          raise NotImplementedError, "Uspec: No block provided for `#{@__uspec_description}`"
         end
       end
     end # initialize
