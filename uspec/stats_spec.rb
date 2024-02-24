fe526d9fd701779ecbc811b533744c0e36816a86
Anthony Cook <github@anthonymcook.com>
2024-02-23 18:34:23 -0600

    Use an instanced object instead of the real test object in the Stats spec to prevent false positives, add explanatory comment to other test

--- uspec/stats_spec.rb
+++ uspec/stats_spec.rb
@@ -1,12 +1,19 @@
 require_relative 'uspec_helper'
 
+def stats_object
+  Uspec::Stats.new
+end
+
 spec 'stats can be inspected' do
+  # this is a regression test
+  # the issue it covers occured because of a refactor where stats had a custom inspect method
+  # stats no longer has a custom inspect method, but this makes sure that nothing breaks
   actual = @__uspec_harness.stats.inspect
   actual.include?("failure") || actual
 end
 
 spec 'stats inspect does not have any stray whitespace' do
-  output = @__uspec_harness.stats.inspect
+  output = stats_object.inspect
   match = output.match /(.*(?:  |\n))/m
   match == nil || match
 end
