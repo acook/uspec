cb7091a44ff6edd10e8e036fc8a7ddb05a85407a
Anthony Cook <github@anthonymcook.com>
2024-02-25 01:47:23 -0600

    Rearrange errors so they better match up with the order they're used

--- lib/uspec/errors.rb
+++ lib/uspec/errors.rb
@@ -2,24 +2,6 @@ module Uspec
   module Errors
     module_function
 
-    def handle_internal_error error, cli = nil
-      message = <<-MSG
-      #{error.class} : #{error.message}
-
-      Uspec encountered an internal error, please report this bug: https://github.com/acook/uspec/issues/new
-
-\t#{error.backtrace.join "\n\t"}
-      MSG
-
-      result = Uspec::Result.new(message, error, true)
-
-      puts
-      warn message
-
-      cli.handle_interrupt! result.raw if cli
-      return result
-    end
-
     def handle_file_error error, path, cli = nil
       if SignalException === error || SystemExit === error then
         exit 3
@@ -39,6 +21,24 @@ module Uspec
         The origin of the error may be in file `#{error_file}` on line ##{error_line}.
 
   \t#{error.backtrace[0,3].join "\n\t"}
+      MSG
+
+      result = Uspec::Result.new(message, error, true)
+
+      puts
+      warn message
+
+      cli.handle_interrupt! result.raw if cli
+      return result
+    end
+
+    def handle_internal_error error, cli = nil
+      message = <<-MSG
+      #{error.class} : #{error.message}
+
+      Uspec encountered an internal error, please report this bug: https://github.com/acook/uspec/issues/new
+
+\t#{error.backtrace.join "\n\t"}
       MSG
 
       result = Uspec::Result.new(message, error, true)
@@ -71,5 +71,6 @@ module Uspec
 \t#{error.backtrace.join "\n\t"}
       MSG
     end
+
   end
 end
