6406902bf660084fd696a3f28b89dfa39ee4cdd7
Anthony Cook <github@anthonymcook.com>
2024-02-28 02:38:17 -0600

    Introduce --full-backtrace and -- options

--- lib/uspec/cli.rb
+++ lib/uspec/cli.rb
@@ -3,8 +3,6 @@ require_relative '../uspec'
 
 class Uspec::CLI
   def initialize args
-    usage unless (args & %w[-h --help -? /? -v --version]).empty?
-
     @paths = args
     @pwd = Pathname.pwd.freeze
     @stats = Uspec::Stats.new
@@ -15,6 +13,9 @@ class Uspec::CLI
   def usage
     warn "uspec v#{::Uspec::VERSION} - minimalistic ruby testing framework"
     warn "usage: #{File.basename $0} [<file_or_path>...]"
+    warn ""
+    warn "\t\t--full_backtrace\tshow full backtrace"
+    warn "\t\t--\tstop checking paths for options (good if a path begins with a dash)"
     exit 1
   end
 
@@ -23,6 +24,7 @@ class Uspec::CLI
   end
 
   def invoke
+    parse_options @paths
     run_specs
     die!
   end
@@ -61,6 +63,8 @@ class Uspec::CLI
   end
 
   def run_paths
+    check_options = true
+
     paths.each do |path|
       run @pwd.join path
     end
@@ -79,6 +83,22 @@ class Uspec::CLI
       harness.file_eval path, line
     else
       warn "path not found: #{path}"
+    end
+  end
+
+  def parse_options args
+    usage unless (args & %w[-h --help -? /? -v --version]).empty?
+
+    args.each_with_index do |arg, i|
+      if arg == '--' then
+        return args
+      elsif arg == '--full_backtrace' then
+        Uspec::Errors.full_backtrace!
+        args.delete_at i
+      elsif arg[0] == ?- then
+        warn "unknown option: #{arg}"
+        args.delete_at i
+      end
     end
   end
 end
